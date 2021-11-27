76544636907f7bd19f1e763765a56c1ab844c83a
Skymorii <hergerthlea@gmail.com>
2021-11-27 14:58:38 +0100

    Fixed mediapipe detection mode in the function for gesture recognition

--- mediapipe_hand_detection/rps_utils.py
+++ mediapipe_hand_detection/rps_utils.py
@@ -44,9 +44,9 @@ def detect_gesture(image):
 
     # Run mediapipe hand detection
     with MP_HANDS.Hands(
+        static_image_mode=True,
         max_num_hands=1,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
+        min_detection_confidence=0.5) as hands:
 
         # Change color model
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
